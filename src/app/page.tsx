b9c2a259ee7a36edc1ce9cb73ff946433e27e454
Henry Agu <aguhenrychuks@gmail.com>
2024-05-07 14:41:55 +0100

    fix: mobile font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,18 @@ export default function Home() {
           <p className="font-bold text-xl">Agu-finance</p>
         </div>
         <div>
-          <button className="bg-primaryButton px-12 py-2 font-medium rounded-md shadow-sm lg:text-base">Login</button>
+          <button className="bg-primaryButton px-12 py-2 font-medium rounded-md shadow-sm lg:text-base">
+            Login
+          </button>
         </div>
       </div>
       <div className="flex flex-col gap-y-10 justify-center items-center mt-40">
         <h1 className="text-black text-center text-5xl w-11/12 font-bold lg:text-7xl lg:tracking-widest lg:leading-none">
           Best finance for <br /> Africa's Professionals
         </h1>
-        <button className="bg-secondaryButton px-16 py-3 font-medium rounded-md shadow-sm lg:text-lg">Login</button>
+        <button className="bg-secondaryButton px-16 py-3 font-medium rounded-md shadow-sm lg:text-lg">
+          Login
+        </button>
       </div>
     </main>
   );
