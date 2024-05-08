275c2b2a5c3ee180946d1f3608b7ba907cb53f27
Henry Agu <aguhenrychuks@gmail.com>
2024-05-08 11:50:34 +0100

    FEAT: login page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="bg-home-background-image bg-no-repeat bg-center bg-cover h-screen w-full">
@@ -6,7 +8,7 @@ export default function Home() {
           <p className="font-bold text-xl">Agu-finance</p>
         </div>
         <div>
-          <button className="bg-primaryButton px-12 py-2 font-medium rounded-md shadow-sm lg:text-base">
+          <button className="bg-primaryButton hover:shadow-lg px-12 py-2 font-medium rounded-md shadow-sm lg:text-base">
             Login
           </button>
         </div>
@@ -15,9 +17,11 @@ export default function Home() {
         <h1 className="text-black text-center text-5xl w-11/12 font-bold lg:text-7xl lg:tracking-widest lg:leading-none">
           Best finance for <br /> Africa's Professionals
         </h1>
-        <button className="bg-secondaryButton px-16 py-3 font-medium rounded-md shadow-sm lg:text-lg">
-          Login
-        </button>
+        <Link href="/login">
+          <button className="bg-secondaryButton hover:shadow-lg px-16 py-3 font-medium rounded-md shadow-sm lg:text-lg">
+            Get started
+          </button>
+        </Link>
       </div>
     </main>
   );
