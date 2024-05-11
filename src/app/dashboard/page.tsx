87205186762ab1a97d5acdf4cd57fbf6b76b752c
Henry Agu <aguhenrychuks@gmail.com>
2024-05-11 20:03:11 +0100

    FEAT: dashboard header

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
-  )
-}
+    <div className="w-full">
+      <div className="p-4 lg:p-6 flex items-center justify-between">
+        <div>
+          <p className="font-bold text-xl">Agu-Finance</p>
+        </div>
+        <div>
+          <button className="bg-black text-white py-2.5 px-8 rounded-md text-sm">Logout</button>
+        </div>
+      </div>
+      <div className="p-4 lg:p-12">
+        <h1 className="text-4xl lg:text-6xl font-bold my-4 lg:my-8">Welcome, Henry</h1>
+        <div>
+          <div>1</div>
+          <div>2</div>
+        </div>
+      </div>
 
-export default page
+    </div>
+  );
+};
+
+export default page;
