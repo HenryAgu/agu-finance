89017c00118c19449003ab10e97cdc69f7295a2a
Henry Agu <aguhenrychuks@gmail.com>
2024-05-16 20:20:31 +0100

    greeting feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { Modal } from "./Modal";
 import Fund from "./Fund";
 import Send from "./Send";
@@ -7,18 +7,33 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/app/lib/store";
 
 const Header = () => {
+  const [greeting, setGreeting] = useState<string>("Welcome");
   const [isSendModalOpen, setIsSendModalOpen] = useState<boolean>(false);
   const [isFundModalOpen, setIsFundModalOpen] = useState<boolean>(false);
 
   const value = useSelector((state: RootState) => state.inputValue.value);
 
   const username = value.charAt(0).toUpperCase() + value.slice(1);
-  const balance = useSelector((state: RootState) => state.balanceValue.value).toLocaleString();
+  const balance = useSelector(
+    (state: RootState) => state.balanceValue.value
+  ).toLocaleString();
+
+  useEffect(() => {
+    let currentTime: number = new Date().getHours();
+    console.log(currentTime);
+    if (currentTime >= 5 && currentTime < 12) {
+      setGreeting("Good morning");
+  } else if (currentTime >= 12 && currentTime < 17) {
+      setGreeting("Good afternoon");
+  } else {
+      setGreeting("Good evening");
+  }
+  }, []);
 
   return (
     <div className="w-full">
-      <h1 className="text-4xl lg:text-6xl font-bold mb-4 lg:mb-8">
-        Welcome Back, {username}
+      <h1 className="capitalize text-4xl lg:text-6xl font-bold mb-4 lg:mb-8">
+        {greeting}, {username}
       </h1>
       <div className="flex flex-col lg:flex-row items-center md:items-start gap-y-5 lg:gap-x-8 mb-4 md:mb-8 lg:mb-8">
         <div className="flex flex-col gap-y-4 p-4 lg:px-8 lg:py-4 shadow-xl border-2 h-fit w-full md:w-[450px] lg:w-[450px] rounded-lg">
