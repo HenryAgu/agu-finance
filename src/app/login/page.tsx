30b7972cee685317a047cc3fbc397ac363819bdd
Henry Agu <aguhenrychuks@gmail.com>
2024-06-03 16:09:37 +0100

    feat: .env secured

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,11 +5,15 @@ import React, { ChangeEvent, FormEvent, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/applications/store";
 import { setInputValue } from "@/applications/inputValue/InputValueSlice";
+import { account } from "../appwrite";
+import { error } from "console";
 
 const page = () => {
+  const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [usernameError, setUsernameError] = useState<boolean>(false);
   const [passwordError, setPasswordError] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
 
   const router = useRouter();
@@ -20,29 +24,37 @@ const page = () => {
     dispatch(setInputValue(e.target.value));
     setUsernameError(false);
   };
+  
 
-  const handleLogin = (e: FormEvent) => {
-    e.preventDefault();
-    if (username === "" && password === "") {
+async function handleLogin(e: FormEvent) {
+    try{
+      await account.createEmailPasswordSession(email, password)
+    }catch(e){
+      console.error(e)
+    }
+    if (username === "" && password === "" && email === "") {
       setUsernameError(true);
       setPasswordError(true);
-    } else if (username.length <= 1 && password.length <= 1) {
+      setEmailError(true)
+    } else if (username.length <= 1 && password.length <= 1 && email.length <= 1) {
       setUsernameError(true);
       setPasswordError(true);
+      setEmailError(true)
     } else if (username === "") {
       setUsernameError(true);
     } else if (password === "") {
       setPasswordError(true);
+    }else if(email === ""){
+      setEmailError(true);
     } else {
       setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-        setUsernameError(false);
-        setPasswordError(false);
-        router.push("/dashboard");
-      }, 2000);
+      setLoading(false);
+      setUsernameError(false);
+      setPasswordError(false);
+      setEmailError(false)
+      router.push("/dashboard");
     }
-  };
+  }
 
   return (
     <div className="flex flex-col gap-y-5 items-center h-full mt-40">
@@ -52,7 +64,6 @@ const page = () => {
       <form
         action=""
         className="flex flex-col gap-y-3.5 border border-black shadow-lg px-6 py-7 rounded-md w-11/12 md:w-3/4 lg:w-1/3"
-        onSubmit={handleLogin}
       >
         <div className="flex flex-col">
           <label htmlFor="" className="mb-1 font-medium text-sm lg:text-base">
@@ -69,6 +80,24 @@ const page = () => {
           {usernameError ? (
             <span className="text-xs text-red-600">
               Username cannot be empty!
+            </span>
+          ) : null}
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="" className="mb-1 font-medium text-sm lg:text-base">
+            Email Address
+          </label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            name="email"
+            className="p-2 border border-black rounded-md text-sm"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {emailError ? (
+            <span className="text-xs text-red-600">
+              Email address cannot be empty!
             </span>
           ) : null}
         </div>
@@ -93,12 +122,18 @@ const page = () => {
             </span>
           ) : null}
         </div>
-        <button className="bg-black text-white p-2.5 rounded-md text-sm">
+        <button
+          className="bg-black text-white p-2.5 rounded-md text-sm"
+          type="button"
+          onClick={handleLogin}
+        >
           {loading ? "Loggin in..." : "Login"}
         </button>
         <span className="text-center text-xs">
           Don't have an account?
-          <Link href="/signup" className="ml-1 text-green-700">Sign up</Link>
+          <Link href="/signup" className="ml-1 text-green-700">
+            Sign up
+          </Link>
         </span>
       </form>
       <p>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 import Link from "next/link";
 import React, { ChangeEvent, FormEvent, useState, useEffect } from "react";
+import { ID, account } from "../appwrite";
 
 const page = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [emailError, setEmailError] = useState<boolean>(false);
   const [passwordError, setPasswordError] = useState<boolean>(false);
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-    setPassword(e.target.value);
-    setEmailError(false);
-    setPasswordError(false);
-  };
+
+  async function handleSignup(e: FormEvent) {
+    try {
+      await account.create(ID.unique(), email, password);
+    } catch (e) {
+      console.error;
+    }
+  }
   return (
     <div className="flex flex-col gap-y-5 items-center h-full mt-40">
       <div>
@@ -27,10 +30,10 @@ const page = () => {
             Email Address
           </label>
           <input
-            type="text"
+            type="email"
             placeholder="Enter your email"
             value={email}
-            name="username"
+            name="email"
             className="p-2 border border-black rounded-md text-sm"
             onChange={(e) => setEmail(e.target.value)}
           />
@@ -58,7 +61,11 @@ const page = () => {
             </span>
           ) : null}
         </div>
-        <button className="bg-black text-white p-2.5 rounded-md text-sm">
+        <button
+          className="bg-black text-white p-2.5 rounded-md text-sm"
+          type="button"
+          onClick={handleSignup}
+        >
           Create account
         </button>
         <div className="flex items-center">
@@ -74,7 +81,9 @@ const page = () => {
         </button>
         <span className="text-center text-xs">
           Already have an account?
-          <Link href="/login" className="ml-1 text-green-700">Login</Link>
+          <Link href="/login" className="ml-1 text-green-700">
+            Login
+          </Link>
         </span>
       </form>
     </div>
